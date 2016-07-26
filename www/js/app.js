8d959d1b606aec170be1d50739e98e1dd7dab29e
Gabriel Viana <gabrielv14@gmail.com>
2016-07-26 15:53:49 -0300

    controllers and routes

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
@@ -21,4 +16,25 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html'
+  })
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+    ;
+    $urlRouterProvider.otherwise("app/home");
 })
